06df820f52fe7e1e96535851f10f6541c7b10b8c
Lariah Christensen <lariah_14@hotmail.com>
2019-07-10 09:22:45 -0600

    part two of snap1

--- snap_7_9/functions.js
+++ snap_7_9/functions.js
@@ -1,18 +1,24 @@
-function operation (num1, num2) {
-	return num1/ num2;
+// // function operation (num1, num2) {
+// // 	return num1/ num2;
+// // }
+// //
+// // let opera = operation(3,6)
+// // console.log (opera)
+// //
+// // console.log(operation(3,6))
+// //
+// // console.log(operation(7,3))
+// // console.log(operation(3,7))
+// // console.log(operation(750,3))
+// //
+// // let punk = (operation(7,3);
+// // let jazz =(operation(3,7);
+// // let ska = (operation(750,3);
+//
+// console.log(punk, ska, jazz);
+
+//this function concatenates two strings
+function concatenateTwoStrings (string1, string2) {
+	return string1 + " " + string2;
 }
-
-let opera = operation(3,6 )
-console.log (opera)
-
-console.log(operation(3,6))
-
-console.log(operation(7,3))
-console.log(operation(3,7))
-console.log(operation(750,3))
-
-let punk = (operation(7,3);
-let jazz =(operation(3,7);
-let ska = (operation(750,3);
-
-console.log(punk, ska, jazz);
+console.log(concatenateTwoStrings('thing0ne', 'thingTwo'));
