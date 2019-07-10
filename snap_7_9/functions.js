2963e2d6377b63ff215a5ad4d272895f7e808f9c
Lariah Christensen <lariah_14@hotmail.com>
2019-07-10 09:10:00 -0600

    snap1

---
+++ snap_7_9/functions.js
@@ -0,0 +1,18 @@
+function operation (num1, num2) {
+	return num1/ num2;
+}
+
+let opera = operation(3,6 )
+console.log (opera)
+
+console.log(operation(3,6))
+
+console.log(operation(7,3))
+console.log(operation(3,7))
+console.log(operation(750,3))
+
+let punk = (operation(7,3);
+let jazz =(operation(3,7);
+let ska = (operation(750,3);
+
+console.log(punk, ska, jazz);
